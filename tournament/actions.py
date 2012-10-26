a7970dc35df4f4072e9ef80ca8340c5ae09340fc
Max Peterson <computmaxer@gmail.com>
2012-10-26 01:51:33 -0500

    Began work on creating tournament models after wizard completes.
    Create Event. Create Tournament.  Still need to figure out Match and Participant objects.

--- tournament/actions.py
+++ tournament/actions.py
@@ -1,7 +1,32 @@
 from tournament import models
 
+import logging
+
 
 def get_events_by_user(user):
     events = models.Event.all().filter('owner =', user).fetch(200)
     #TODO: account for the admins who should also be able to see the event in their list.
     return events
+
+
+def create_tournament(form_data, p_form_data, user):
+    f=form_data
+    p=p_form_data
+    e = models.Event(
+        name=f.get('name'),
+        date=f.get('date'),
+        location=f.get('location'),
+        owner=user,
+        perms=f.get('tournament_security'))
+    logging.warning(e)
+    e.put()
+    t = models.Tournament(
+        type=f.get('type'),
+        order=0,
+        win_method=models.Tournament.HIGHEST_WINS,
+        parent=e)
+    t.put()
+    logging.warning(t)
+
+    #TODO: Determine Math objects and coresponding participant objects based on seeds.
+
--- tournament/views.py
+++ tournament/views.py
@@ -3,10 +3,13 @@ from flask.templating import render_template
 
 from lib.flask_login import login_required
 
+import wtforms
 import auth
 import forms
 import models
 import actions
+
+import logging
 
 class New_Tournament(auth.UserAwareView):
     def get(self):
@@ -17,6 +20,8 @@ class New_Tournament(auth.UserAwareView):
         return self.render_new_tourney(context)
 
     def post(self):
+        #Initialize context with empty fields dicts. We pass values from one step to the next in the
+        #fields dict, which is rendered as hidden inputs and comes back in request.form in each step
         context = self.get_context({'fields':{}})
         step = int(request.form.get('step'))
         if step == 1:
@@ -30,19 +35,38 @@ class New_Tournament(auth.UserAwareView):
                 return self.render_new_tourney({'fields':{'step':1}, 'form':form})
         elif step == 2:
             form = forms.NewTournamentStep2(request.form, prefix="step2")
+            step1form = forms.NewTournamentStep1(request.form)
+            context['fields'].update(step1form.data)
             if form.validate():
                 context['fields'].update(form.data)
-                context['fields'].update({
-                    'tournament_security':request.form.get('tournament_security')})
                 context['fields'].update({'step':3})
                 context['form'] = forms.NewTournamentStep3(prefix="step3")
-                #TODO finish processing
-
                 return self.render_new_tourney(context)
             else:
-                return self.render_new_tourney({'fields':{'step':2}, 'form':form})
+                context.update({'fields':{'step':2}, 'form':form})
+                return self.render_new_tourney(context)
         elif step == 3:
-            pass
+            form = forms.NewTournamentStep3(request.form, prefix="step3")
+            step2form = forms.NewTournamentStep2(request.form)
+            step1form = forms.NewTournamentStep1(request.form)
+            context['fields'].update(step2form.data)
+            context['fields'].update(step1form.data)
+            if form.validate():
+                context['fields'].update(form.data)
+                num_participants = context['fields'].get('number_participants')
+                include_seeds = context['fields'].get('show_seeds')
+
+                p_form = forms.handle_participant_forms(request.form,num_participants,include_seeds)
+                p_form.validate() #TODO: if here?
+                logging.warning(p_form.data) #TODO: REMOVE
+
+                actions.create_tournament(context['fields'], p_form.data, self.user)
+
+                #TEMP REDIRECT TO HOME TODO: Redirect to Tournament Overview Page once it exists
+                return render_template('home.html', **self.get_context())
+            else:
+                context.update({'fields':{'step':3}, 'form':form})
+                return self.render_new_tourney(context)
 
     #Made this so I don't have to type the template a bunch of times
     def render_new_tourney(self, context):
