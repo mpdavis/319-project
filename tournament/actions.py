8cca7b80e4a5d3f1e14cda9a811106f3d1b55f3b
Max Peterson <computmaxer@gmail.com>
2012-10-27 16:12:39 -0500

    Models are now created when the wizard completes.

--- tournament/actions.py
+++ tournament/actions.py
@@ -1,3 +1,5 @@
+from google.appengine.ext import db
+
 from tournament import models
 
 import logging
@@ -18,7 +20,6 @@ def create_tournament(form_data, p_form_data, user):
         location=f.get('location'),
         owner=user,
         perms=f.get('tournament_security'))
-    logging.warning(e)
     e.put()
     t = models.Tournament(
         type=f.get('type'),
@@ -26,7 +27,39 @@ def create_tournament(form_data, p_form_data, user):
         win_method=models.Tournament.HIGHEST_WINS,
         parent=e)
     t.put()
-    logging.warning(t)
 
-    #TODO: Determine Math objects and coresponding participant objects based on seeds.
+    name_dict = {}
+    seeds = []
+    if form_data.get('show_seeds', False):
+        raw_seeds = []
+        for field, value in p_form_data.items():
+            if 'seed' in field:
+                num = field[11:-4]
+                raw_seeds.append((value, num))
+            if 'name' in field:
+                num = field[11:-4]
+                name_dict[num] = value
+        raw_seeds.sort()
+        for raw in raw_seeds:
+            seeds.append(raw[1])
 
+    else:
+        for field, value in p_form_data.items():
+            if 'name' in field:
+                num = field[11:-4]
+                name_dict[num] = value
+                seeds.append(num)
+
+    logging.warning(seeds)
+    #TODO Optimize puts
+    ps_to_put = []
+    round = 1
+    for i in range(len(seeds)/2):
+        m = models.Match(round=round, has_been_played=False, parent=t)
+        m.put()
+        p1 = models.Participant(seed=int(seeds[i]), name=name_dict.get(seeds[i]), parent=m, event_key=e.key())
+        p2 = models.Participant(seed=int(seeds[len(seeds)-i-1]), name=name_dict.get(seeds[len(seeds)-i-1]), parent=m, event_key=e.key())
+        ps_to_put.append(p1)
+        ps_to_put.append(p2)
+        round += 1
+    db.put(ps_to_put)
