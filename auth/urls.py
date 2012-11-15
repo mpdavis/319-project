8151c12da1f5ff8373d89ff0a76864b6e3df68f3
Michael Davis <mike.davis@webfilings.com>
2012-11-15 15:12:34 -0600

    Fixing urls

--- auth/urls.py
+++ auth/urls.py
@@ -3,16 +3,16 @@ from auth import views as auth_views
 
 def setup_urls(app):
     """URLs for the auth module"""
-    app.add_url_rule('/auth/login/', view_func=auth_views.login.as_view('login'))
-    app.add_url_rule('/auth/logout/', view_func=auth_views.logout.as_view('logout'))
-    app.add_url_rule('/auth/register/', view_func=auth_views.register.as_view('register'))
+    app.add_url_rule('/auth/login/', view_func=auth_views.Login.as_view('login'))
+    app.add_url_rule('/auth/logout/', view_func=auth_views.Logout.as_view('logout'))
+    app.add_url_rule('/auth/register/', view_func=auth_views.Register.as_view('register'))
     app.add_url_rule('/auth/check_username/', view_func=auth_views.check_username.as_view('check_username'))
-    app.add_url_rule('/auth/welcome/', view_func=auth_views.welcome.as_view('welcome'))
+    app.add_url_rule('/auth/welcome/', view_func=auth_views.Welcome.as_view('welcome'))
 
     # Facebook Oauth URLs
-    app.add_url_rule('/auth/facebook_login', view_func=auth_views.facebook_login.as_view('facebook_login'))
-    app.add_url_rule('/auth/facebook_authorized', view_func=auth_views.facebook_authorized.as_view('facebook_authorized'))
+    app.add_url_rule('/auth/facebook_login', view_func=auth_views.FacebookLogin.as_view('facebook_login'))
+    app.add_url_rule('/auth/facebook_authorized', view_func=auth_views.FacebookAuthorized.as_view('facebook_authorized'))
 
     # Google Oauth URLs
-    app.add_url_rule('/auth/google_login', view_func=auth_views.google_login.as_view('google_login'))
-    app.add_url_rule('/auth/google_authorized', view_func=auth_views.google_authorized.as_view('google_authorized'))
+    app.add_url_rule('/auth/google_login', view_func=auth_views.GoogleLogin.as_view('google_login'))
+    app.add_url_rule('/auth/google_authorized', view_func=auth_views.GoogleAuthorized.as_view('google_authorized'))
