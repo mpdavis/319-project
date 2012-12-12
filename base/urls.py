b369588a032b1afc7c43d3697068db0a9c5dd019
Max Peterson <computmaxer@gmail.com>
2012-12-12 15:36:55 -0600

    Added logging to 500 and 404 error handlers.
    Added sort images for data tables.

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from base import views as base_views
 from flask import render_template
+import logging
 
 
 def setup_urls(app):
@@ -11,8 +12,10 @@ def setup_urls(app):
 
     @app.errorhandler(404)
     def page_not_found(e):
+        logging.error(e)
         return render_template('404.html'), 404
 
     @app.errorhandler(500)
     def server_error(e):
+        logging.error(e)
         return render_template('500.html'), 500
